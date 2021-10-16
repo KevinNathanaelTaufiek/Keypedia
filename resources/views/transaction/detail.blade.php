bcc2747421d032d7c729c4558844f2c4da3820d1
KevinNathanaelTaufiek <kevin.taufiek@gmail.com>
2021-10-16 19:04:17 +0700

    small changes

--- resources/views/transaction/detail.blade.php
+++ resources/views/transaction/detail.blade.php
@@ -8,33 +8,6 @@
     $total = 0;
 @endphp
 
-<<<<<<< HEAD
-<table border="1" class="margin-bot">
-    <tr>
-        <th>Keyboard Image</th>
-        <th>Keyboard Name</th>
-        <th>Subtotal</th>
-        <th>Quantity</th>
-    </tr>
-    @foreach ($transactionDetails as $transactionDetail)
-    <tr>
-        <td><img class="margin-all" src="{{ asset('storage/'. $transactionDetail->keyboard->keyboardImage) }}" alt="keyboard-image" width="300px" height="200px"></td>
-        <td><p>{{ $transactionDetail->keyboard->keyboardName }}</p></td>
-        <td><p>{{ $transactionDetail->keyboard->keyboardPrice * $transactionDetail->qty }}</p></td>
-        <td><p>{{ $transactionDetail->qty }}</p></td>
-    </tr>
-        @php
-            $total += $transactionDetail->keyboard->keyboardPrice * $transactionDetail->qty;
-        @endphp
-    @endforeach
-    <tr>
-        <th></th>
-        <th></th>
-        <th>Total Price:</th>
-        <th>${{ $total }}</th>
-    </tr>
-</table>
-=======
 <div class="container">
     <div class="col">
         <div class="col-11">
@@ -78,5 +51,4 @@
         </div>
     </div>
 </div>
->>>>>>> 73a6fbd6c72ddea56bfcceb7f08172e7161a9f6c
 @endsection
